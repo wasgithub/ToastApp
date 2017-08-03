da04c9f91994163b5ee97096055cde4d8b77d496
washington <was.alexandre42@gmail.com>
2017-08-02 22:04:41 -0300

    Initial commit

---
+++ app/src/main/java/br/com/washington/toastapp/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.washington.toastapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import br.com.washington.minhabiblioteca.CustomToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toast(View view) {
+        new CustomToast().showToast(this, "teste");
+    }
+}
